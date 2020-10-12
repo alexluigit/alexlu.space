d5eadc8ac598485c28be98792d279094a4d1dc07
Alex <hellosimon1103@hotmail.com>
2020-10-13 06:07:54 +0800

    feat(layouts): add navigation components

---
+++ components/Navigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/blogs">Posts</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/wiki">Wiki</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
