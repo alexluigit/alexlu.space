f62ee542e1d0e23b3338faf2118befb9cd716dbf
Alex <hellosimon1103@hotmail.com>
2020-10-13 07:04:39 +0800

    feat(pages): add '/blogs', '/blog/:slug' pages

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    <h1>{{ article.title }}</h1>
+    <nuxt-content :document="article" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('blog', params.slug).fetch()
+    console.log(article)
+    return { article }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/blogs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <nuxt-link v-for="blog in blogs" :to="`/blog/${blog.slug}`">
+      <h3>{{blog.title}}</h3>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const blogs = await $content('blog').only(['title', 'slug']).fetch()
+    console.log(blogs)
+    return {blogs}
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
