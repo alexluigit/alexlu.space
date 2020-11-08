0afebe01a4c2e581055d7d4626e13e7d74e40f43
Alex <hellosimon1103@hotmail.com>
2020-11-08 11:26:11 +0800

    refactor(pages): use index page
    Move pages/blogs.vue and pages/wikis.vue to pages/blog/index.vue and
    pages/wiki/index.vue

---
+++ pages/wiki/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <nuxt-link v-for="wiki in wikis" :key="wiki.slug" :to="`/wiki/${wiki.slug}`">
+    <h3>{{wiki.title}}</h3>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const wikis = await $content('wiki').only(['title', 'slug']).fetch()
+    console.log(wikis)
+    return { wikis }
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
