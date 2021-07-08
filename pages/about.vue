38766bb593b610c1796af18dfb2b9d084369b4bf
Alex <hellosimon1103@hotmail.com>
2021-07-08 17:36:21 +0800

    feat: add about page

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>About me</h1>
+    <h1>This blog</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
